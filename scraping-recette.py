a5d2137708f30e60e1bdf0701ce796d26fd06ee3
Lorenzo <lorenzo-a@live.fr>
2024-01-21 19:58:22 +0100

    Add bs

--- scraping-recette.py
+++ scraping-recette.py
@@ -1,7 +1,26 @@
+from bs4 import BeautifulSoup
 import requests
 
 url = 'https://codeavecjonathan.com/scraping/recette/'
 
 response = requests.get(url)
+response.encoding = response.apparent_encoding
 
-print("FIN")
+if response.status_code == 200:
+
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    title_content = soup.title.string
+
+    print(title_content)
+
+    # f = open("recette.html", "w")
+    # f.write(html)
+    # f.close()
+
+else:
+    print("Erreur :", response.status_code)
+
+print(response)
