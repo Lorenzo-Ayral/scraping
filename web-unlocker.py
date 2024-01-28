c27777950aec12b936f76047677484d1a58fe1d8
Lorenzo <lorenzo-a@live.fr>
2024-01-28 20:30:22 +0100

    Add dir

--- web-unlocker.py
+++ web-unlocker.py
@@ -1,4 +1,3 @@
-# import sys
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 import os
@@ -18,25 +17,25 @@ try:
 except FileNotFoundError:
     counter = 0
 
+directory = "woodbrass"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 url = 'https://www.woodbrass.com/guitares/guitares+electriques/retro+vintage#filtre-liste-categ'
 parsed_url = urlparse(url)
 domain_name = parsed_url.netloc.split('.')[1]
-output_filename = f'{domain_name}{counter}.html'
-
-with open(output_filename, 'w') as f:
-    f.write('Hello world')
-
-with open('counter.txt', 'w') as f:
-    f.write(str(counter + 1))
-
+output_filename = os.path.join(directory, f'{domain_name}{counter}.html')
 
 opener = urllib.request.build_opener(
     urllib.request.ProxyHandler({
         'http': http_proxy,
         'https': https_proxy
     }))
-html = opener.open(url).read().decode('utf-8')
 
-f = open("web-unlocker.html", "w")
+html = opener.open(url).read().decode('utf-8')
+f = open(output_filename, "w")
 f.write(html)
 f.close()
+
+with open('counter.txt', 'w') as f:
+    f.write(str(counter + 1))
