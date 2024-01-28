16e72581f09180f861ff5f33657a8f2cdbcece45
Lorenzo <lorenzo-a@live.fr>
2024-01-28 21:23:17 +0100

    Add bypass

--- scraping-browser.py
+++ scraping-browser.py
@@ -6,40 +6,43 @@ from playwright.async_api import async_playwright
 
 SBR_WS_CDP = 'wss://brd-customer-hl_d532bd73-zone-scraping_browser1:g1n2qxu9rdg0@brd.superproxy.io:9222'
 
-
-try:
-    with open('counter.txt', 'r') as f:
-        content = f.read()
-        counter = int(content) if content else 0
-except FileNotFoundError:
-    counter = 0
-
 url ="https://www.codeavecjonathan.com/scraping/techsport/"
 parsed_url = urlparse(url)
 domain_name = parsed_url.netloc.split('.')[1]
 directory = domain_name
 if not os.path.exists(directory):
     os.makedirs(directory)
-output_filename = os.path.join(directory, f'{domain_name}{counter}.html')
+output_filename = os.path.join(directory, f'{domain_name}.html')
+
+BYPASS_SCRAPING = os.path.exists(output_filename)
 
 async def run(pw):
-    print('Connecting to Scraping Browser...')
-    browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
+    if not BYPASS_SCRAPING:
+        print('Connecting to Scraping Browser...')
+        browser = await pw.chromium.connect_over_cdp(SBR_WS_CDP)
     try:
-        page = await browser.new_page()
-        print('Connected! Navigating to ' + url + '...')
-        await page.goto(url)
-        await page.screenshot(path='./'+ directory +'/scraping-browser' + str(counter) + '.png', full_page=True)
-        print('Navigated! Scraping page content...')
-        html = await page.content()
-        f = open(output_filename, "w")
-        f.write(html)
-        f.close()
+        if not BYPASS_SCRAPING:
+            page = await browser.new_page()
+            print('Connected! Navigating to ' + url + '...')
+            await page.goto(url)
+            await page.screenshot(path='./'+ directory +'/scraping-browser.png', full_page=True)
+            print('Navigated! Scraping page content...')
+            html = await page.content()
+            f = open(output_filename, "w")
+            f.write(html)
+            f.close()
 
-        with open('counter.txt', 'w') as f:
-            f.write(str(counter + 1))
+        else:
+            print('Bypassing scraping...')
+            if os.path.isfile(output_filename):
+                f = open(output_filename, "r")
+                html = f.read()
+                f.close()
+            else:
+                print(f"File {output_filename} does not exist.")
     finally:
-        await browser.close()
+        if not BYPASS_SCRAPING:
+            await browser.close()
 
 
 async def main():
